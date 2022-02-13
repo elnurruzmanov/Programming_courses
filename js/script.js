2173d6430d037b43f2559a2cdf83d4aa2f9316a2
elnurruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-02-14 00:34:57 +0300

    s

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict"
+
+//documents
+
+const language = document.querySelectorAll(".language");
+const ru = document.querySelector(".ru")
+const uz = document.querySelector(".uzb")
+const eng = document.querySelector(".eng")
+const down = document.querySelector(".fa-caret-down")
+const up = document.querySelector(".fa-caret-up")
+const play = document.querySelector(".fa-play")
+const languages = document.querySelector(".languages");
+const lang = document.querySelector(".lang");
+const rus = document.querySelector(".rus");
+const uzbb = document.querySelector(".uzbb");
+
+//Functions Event Listeners
+
+const plays = ()=>{
+    play.addEventListener("click", ()=>{
+        languages.style.display="block"
+        down.style.display = "block"
+        play.style.display = "none"
+    })
+    down.addEventListener("click", ()=>{
+        languages.style.display="none"
+        down.style.display = "none"
+        play.style.display = "block"
+    })
+}
+
+plays();
+
+
+//Languages
+
+const langs = ()=>{
+    rus.addEventListener("click", ()=>{
+            eng.style.display = "none"
+            ru.style.display = "block"
+            languages.style.display = "none"
+            down.style.display = "none"
+            play.style.display = "block"
+    })
+    uzbb.addEventListener("click", ()=>{
+        eng.style.display = "none"
+        uz.style.display = "block"
+        ru.style.display = "none"
+        languages.style.display = "none"
+        down.style.display = "none"
+        play.style.display = "block"
+    })
+}
+langs()
+
+
+
+
+const append = document.querySelector(".li")
+const navList = todoList.children
+
+append.addEventListener("click", function(){
+
+})
